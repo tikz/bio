8d32560b904f86c112c82c52e8d9280fdf356229
Mauro <tik@lag.party>
2020-10-10 15:32:57 -0300

    Add CloseResidues

--- pdb/calc.go
+++ pdb/calc.go
@@ -22,3 +22,15 @@ func ResiduesDistance(r1 *Residue, r2 *Residue) float64 {
 func AtomsDistance(a1 *Atom, a2 *Atom) float64 {
 	return math.Sqrt(math.Pow(a1.X-a2.X, 2) + math.Pow(a1.Y-a2.Y, 2) + math.Pow(a1.Z-a2.Z, 2))
 }
+
+// CloseResidues returns all residues within a given distance from the specified residue.
+func CloseResidues(p *PDB, r *Residue, distance float64) (residues []*Residue) {
+	for _, chain := range p.Chains {
+		for _, res := range chain {
+			if res != r && ResiduesDistance(res, r) < distance {
+				residues = append(residues, res)
+			}
+		}
+	}
+	return
+}
