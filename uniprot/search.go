56aaedf455ea1f03074e7161c69e7dc104076711
Mauro <tik@lag.party>
2020-10-17 16:32:48 -0300

    Add URL escape

--- uniprot/search.go
+++ uniprot/search.go
@@ -2,6 +2,7 @@ package uniprot
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/tikz/bio/http"
@@ -9,7 +10,8 @@ import (
 
 // SearchQuery fetches the search results for a given query and returns a list of UniProt IDs.
 func SearchQuery(query string) (ids []string, err error) {
-	url := "https://www.uniprot.org/uniprot/?query=" + query + "&format=list"
+	url := "https://www.uniprot.org/uniprot/?query=" + url.QueryEscape(query) + "&format=list"
+	fmt.Println(url)
 	raw, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("UniProt search query %v: %v", query, err)
