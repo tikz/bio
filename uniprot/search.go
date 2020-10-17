fa0dc35880cc92646e0e4d8986aace7f9537cd43
Mauro <tik@lag.party>
2020-10-17 16:33:36 -0300

    Remove print

--- uniprot/search.go
+++ uniprot/search.go
@@ -11,7 +11,6 @@ import (
 // SearchQuery fetches the search results for a given query and returns a list of UniProt IDs.
 func SearchQuery(query string) (ids []string, err error) {
 	url := "https://www.uniprot.org/uniprot/?query=" + url.QueryEscape(query) + "&format=list"
-	fmt.Println(url)
 	raw, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("UniProt search query %v: %v", query, err)
